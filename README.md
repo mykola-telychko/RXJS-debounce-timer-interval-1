40a580a015597615bef1c0bb10567a07528dbadb
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-19 23:36:49 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RXJS-debounce-timer-interval-1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-65q3bn)
---
+++ index.ts
@@ -0,0 +1,15 @@
+import { interval, timer } from 'rxjs';
+import { debounce } from 'rxjs/operators';
+
+//emit value every 1 second, ex. 0...1...2
+const interval$ = interval(2000);
+//raise the debounce time by 200ms each second
+const debouncedInterval = interval$.pipe(debounce((val) => timer(val * 200)));
+/*
+  After 5 seconds, debounce time will be greater than interval time,
+  all future values will be thrown away
+  output: 0...1...2...3...4......(debounce time over 1s, no values emitted)
+*/
+const subscribe = debouncedInterval.subscribe((val) =>
+  console.log(`Example Two: ${val}`)
+);
